875743d30f42ee0d175c0347e07d0160e6dd6820
Сидельников Станислав <stanislav.k.sidelnikov@gazprombank.ru>
2023-12-28 12:42:27 +0600

    Initial Commit

---
+++ PodsVersionsMerge/PodsVersionsMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodsVersionsMergeApp.swift
+//  PodsVersionsMerge
+//
+//  Created by Stan Sidel on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PodsVersionsMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
